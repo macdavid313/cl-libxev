62992cab69c5e842cfccc245d0518edfd871bd05
Tianyu Gu <gty@CLOS.org>
2024-10-25 17:29:57 +1100

    add grovel.lisp for aarch64-linux

---
+++ src/grovel/aarch64-linux/grovel.lisp
@@ -0,0 +1,37 @@
+;;;; grovel.cl
+;;;; DO NOT MODIFY: this file has been automatically generated.
+(in-package #:cl-libxev)
+
+;;; Opaque types.
+#+allegro (ff:def-foreign-type xev-loop (:array :unsigned-char 512))
+#-allegro (cffi:defctype xev-loop (:array :unsigned-char 512))
+#+allegro (ff:def-foreign-type xev-completion (:array :unsigned-char 320))
+#-allegro (cffi:defctype xev-completion (:array :unsigned-char 320))
+#+allegro (ff:def-foreign-type xev-watcher (:array :unsigned-char 256))
+#-allegro (cffi:defctype xev-watcher (:array :unsigned-char 256))
+#+allegro (ff:def-foreign-type xev-threadpool (:array :unsigned-char 64))
+#-allegro (cffi:defctype xev-threadpool (:array :unsigned-char 64))
+#+allegro (ff:def-foreign-type xev-threadpool-batch (:array :unsigned-char 32))
+#-allegro (cffi:defctype xev-threadpool-batch (:array :unsigned-char 32))
+#+allegro (ff:def-foreign-type xev-threadpool-task (:array :unsigned-char 32))
+#-allegro (cffi:defctype xev-threadpool-task (:array :unsigned-char 32))
+#+allegro (ff:def-foreign-type xev-threadpool-config (:array :unsigned-char 64))
+#-allegro (cffi:defctype xev-threadpool-config (:array :unsigned-char 64))
+
+;;; Callback types.
+#+allegro (ff:def-foreign-type xev-cb-action :int)
+#-allegro (cffi:defctype xev-cb-action :int)
+(defconstant +XEV-DISARM+ 0)
+(defconstant +XEV-REARM+ 0)
+
+#+allegro (ff:def-foreign-type xev-run-mode-t :int)
+#-allegro (cffi:defctype xev-run-mode-t :int)
+(defconstant +XEV-RUN-NO-WAIT+ 0)
+(defconstant +XEV-RUN-ONCE+ 1)
+(defconstant +XEV-RUN-UNTIL-DONE+ 2)
+
+#+allegro (ff:def-foreign-type xev-completion-state-t :int)
+#-allegro (cffi:defctype xev-completion-state-t :int)
+(defconstant +XEV-COMPLETION-DEAD+ 0)
+(defconstant +XEV-COMPLETION-ACTIVE+ 1)
+
